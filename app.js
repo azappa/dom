ecde7c581d29fcc86975d2d4d065ca647dc46e53
Alessio Zappa <alessioedoardozappa@gmail.com>
2019-01-06 18:59:38 +0100

    updating dependencies

--- app.js
+++ app.js
@@ -1,167 +1,140 @@
-/*jshint node:true, eqnull:true, laxcomma:true, undef:true, indent:2, camelcase:false */
-'use strict';
+const http = require('http');
+const url = require('url');
+const request = require('request');
+const cheerio = require('cheerio');
+const Sugar = require('sugar');
 
+Sugar.extend();
 
-var http = require('http')
-  , url = require('url')
-  , request = require('request')
-  , cheerio = require('cheerio')
-  ;
-
-require('sugar');
-
-
-http.createServer(function (req, res) {
-
+http.createServer((req, res) => {
   //  -- getting url from url parameter in address bar (if not get a default address :D)
-  var pageURL = url.parse(req.url, true).query.url || 'http://www.alessiozappa.com'
-    , pageElement = url.parse(req.url, true).query.element || '*'
-    , outputObj
-    ;
+  const pageURL = url.parse(req.url, true).query.url || 'http://www.alessiozappa.com';
+  const pageElement = url.parse(req.url, true).query.element || '*';
+  let outputObj = null;
 
 
-  request(pageURL, function (e, r, b) {
-
+  request(pageURL, (e, r, b) => {
     if (e) {
       //  -- got an error --
-      outputObj = {'error': 'undefined code error'};
+      outputObj = { error: 'undefined code error' };
       res.end(JSON.stringify(outputObj));
-
+      return;
+    }
+    //  -- status code is not ok --
+    if (r.statusCode !== 200) {
+      outputObj = { error: 'code ' + r.statusCode };
+      res.end(JSON.stringify(outputObj));
     } else {
-      //  -- status code is not ok --
-      if (r.statusCode !== 200) {
-        
-        outputObj = {'error': 'code ' + r.statusCode};
-        res.end(JSON.stringify(outputObj));
-
-      } else {
-        //  -- everything went smooth (i hope!) --
-        var $ = cheerio.load(b)
-          , head = $.root().find('head').children()
-          , body = pageElement === '*' ? $.root().find('body').children() : $(pageElement)
-          , checkTags = ['src', 'href']
-          , elements = pageElement === '*' ? [[head, 'head'], [body, 'body']] : [[body, 'body']]
-          , iterate
-          , replaceRelativeUrls
-          , buildObjects
-          ;
+      //  -- everything went smooth (i hope!) --
+      const $ = cheerio.load(b);
+      const head = $.root().find('head').children();
+      const body = pageElement === '*' ? $.root().find('body').children() : $(pageElement);
+      const checkTags = ['src', 'href'];
+      const elements = pageElement === '*' ? [[head, 'head'], [body, 'body']] : [[body, 'body']];
+      let iterate = null;
+      let replaceRelativeUrls = null;
+      let buildObjects = null;
 
 
-        //  -- replace checkTags values with absolute path urls --
-        replaceRelativeUrls = function (el) {
-          checkTags.each(function (tag) {
-            if (Object.has(el.attribs, tag)) {
-              if (typeof(el.attribs[tag] !== 'undefined' && el.attribs[tag] !== undefined)) {
-                if (el.attribs[tag].startsWith(new RegExp('^(?:[a-z]+:)?//', 'i'))) {
-                  return;
-                }
-                el.attribs[tag] = (pageURL + '/' + el.attribs[tag]).replace('//', '/');
+      //  -- replace checkTags values with absolute path urls --
+      replaceRelativeUrls = (el) => {
+        checkTags.forEach((tag) => {
+          if (Object.has(el.attribs, tag)) {
+            if (typeof el.attribs[tag] !== 'undefined' && el.attribs[tag] !== undefined) {
+              if (
+                el.attribs[tag].startsWith('//')
+                || el.attribs[tag].startsWith('http://')
+                || el.attribs[tag].startsWith('https://')
+              ) {
+                return;
               }
+              el.attribs[tag] = (pageURL + '/' + el.attribs[tag]).replace('//', '/');
             }
-          });
-        };
+          }
+        });
+      };
 
 
-        //  -- cycling through children --
-        iterate = function (el, depth, arrayToAppend) {
+      //  -- cycling through children --
+      iterate = (el, depth, arrayToAppend) => {
+        if ($(el).children().length > 0) {
+          $(el).children().each((i, c) => {
+            replaceRelativeUrls(c);
 
-          if ($(el).children().length > 0) {
-            $(el).children().each(function (i, c) {
-
-              replaceRelativeUrls(c);
-
-              var tObj = {
-                'tag': c.name,
-                'attrib': c.attribs,
-                'depth': depth + 1,
-                'children': []
-              };
-
-              arrayToAppend.push(tObj);
-              iterate($(c), depth + 1, tObj.children);
-            });
-          } else {
-
-            var tObj = {
-              'depth': depth + 1,
-              'children': 0,
-              'value': $(el).text()
+            const tObj = {
+              tag: c.name,
+              attrib: c.attribs,
+              depth: depth + 1,
+              children: []
             };
 
             arrayToAppend.push(tObj);
-          }
-        };
-
-
-        outputObj = {
-          'url': pageURL,
-          'element': pageElement
-        };
-        if (pageElement === '*') {
-          outputObj.head = [];
-          outputObj.body = [];
+            iterate($(c), depth + 1, tObj.children);
+          });
         } else {
-          outputObj.body = [];
+          const tObj = {
+            depth: depth + 1,
+            children: 0,
+            value: $(el).text()
+          };
+
+          arrayToAppend.push(tObj);
         }
+      };
 
 
-        //  -- build each obj (head + body) --
-        buildObjects = function (o) {
-
-          if (o[0].length > 0) {
-            $(o[0]).each(function (i, el) {
-              var _depth = 0
-                ;
-
-              if (el.name) {
-                replaceRelativeUrls(el);
-
-                var objToCreate = {
-                  'tag': el.name,
-                  'attr': el.attribs,
-                  'depth': _depth,
-                  'children': []
-                };
-
-                if (pageElement !== '*') {
-                  objToCreate.value = $(el).text();
-                } else {
-                  iterate($(el), _depth, objToCreate.children);
-                }
-                outputObj[o[1]].push(objToCreate);
-              } else {
-                return;
-              }
-            });
-
-          } else {
-            
-            outputObj[o[1]].push(
-              [
-                {
-                  'html': $.root().find([o[1]]).html(),
-                }
-              ]
-            );
-
-          }
-        };
-
-
-        //  -- build final object --
-        elements.each(function (element) {
-          buildObjects(element);
-        });
-
-
-        //  -- send results --
-        res.end('<pre style="font-size: 11px;">' + JSON.stringify(outputObj, null, 2) + '</pre>', 'utf-8');
-
+      outputObj = {
+        url: pageURL,
+        element: pageElement
+      };
+      if (pageElement === '*') {
+        outputObj.head = [];
+        outputObj.body = [];
+      } else {
+        outputObj.body = [];
       }
 
+
+      //  -- build each obj (head + body) --
+      buildObjects = (o) => {
+        if (o[0].length > 0) {
+          $(o[0]).each((i, el) => {
+            let depth = 0;
+
+            if (el.name) {
+              replaceRelativeUrls(el);
+
+              const objToCreate = {
+                tag: el.name,
+                attr: el.attribs,
+                depth,
+                children: []
+              };
+
+              if (pageElement !== '*') {
+                objToCreate.value = $(el).text();
+              } else {
+                iterate($(el), depth, objToCreate.children);
+              }
+              outputObj[o[1]].push(objToCreate);
+            }
+          });
+        } else {
+          outputObj[o[1]].push(
+            [{ html: $.root().find([o[1]]).html() }]
+          );
+        }
+      };
+
+
+      //  -- build final object --
+      elements.forEach((element) => {
+        buildObjects(element);
+      });
+
+
+      //  -- send results --
+      res.end('<pre style="font-size: 11px;">' + JSON.stringify(outputObj, null, 2) + '</pre>', 'utf-8');
     }
-
   });
-
-
 }).listen(1337, '127.0.0.1');
